7cea4681266ccf8c3b8d22e95e4547241ff2a522
Muugii Munkhbold <mmunkhbold94@gmail.com>
2023-02-07 20:26:44 -0700

    finished models

--- server/models/Book.js
+++ server/models/Book.js
@@ -2,11 +2,7 @@ const { Schema } = require('mongoose');
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const bookSchema = new Schema({
-  authors: [
-    {
-      type: String,
-    },
-  ],
+  authors: [String],
   description: {
     type: String,
     required: true,
@@ -16,12 +12,8 @@ const bookSchema = new Schema({
     type: String,
     required: true,
   },
-  image: {
-    type: String,
-  },
-  link: {
-    type: String,
-  },
+  image: String,
+  link: String,
   title: {
     type: String,
     required: true,
